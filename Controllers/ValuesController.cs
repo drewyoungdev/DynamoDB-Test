5715793f3ba8ae5316e56f25fef6246e2f5d3168
Andrew Young <young.andrew2@outlook.com>
2019-06-09 12:39:21 -0400

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dynamodb_test.Controllers
+{
+    // Do `docker run dynamodb-local` (after downloading latest dynamodb/local dockerhub image)
+    // Create table with GET http://localhost:5000/api/values/init
+    // Add item to table with POST http://localhost:5000/api/values/ with body '{ "Id": 1, "Title": "test" }
+    // Retrieve item from table with GET http://localhost:5000/api/values/1 and it should return "test"
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private const string TableName = "SampleData";
+
+        private readonly IAmazonDynamoDB dynamoDb;
+
+        public ValuesController(IAmazonDynamoDB dynamoDb)
+        {
+            this.dynamoDb = dynamoDb;
+        }
+
+        [HttpGet("init")]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            var request = new ListTablesRequest
+            {
+                Limit = 10
+            };
+
+            var response = await dynamoDb.ListTablesAsync(request);
+
+            var results = response.TableNames;
+
+            if (!results.Contains(TableName))
+            {
+                var createRequest = new CreateTableRequest
+                {
+                    TableName = TableName,
+                    AttributeDefinitions = new List<AttributeDefinition>
+                    {
+                        new AttributeDefinition
+                        {
+                            AttributeName = "Id",
+                            AttributeType = "N"
+                        }
+                    },
+                    KeySchema = new List<KeySchemaElement>
+                    {
+                        new KeySchemaElement
+                        {
+                            AttributeName = "Id",
+                            KeyType = "HASH"  //Partition key
+                        }
+                    },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 2,
+                        WriteCapacityUnits = 2
+                    }
+                };
+
+                await dynamoDb.CreateTableAsync(createRequest);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var request = new GetItemRequest
+            {
+                TableName = TableName,
+                Key = new Dictionary<string, AttributeValue> { { "Id", new AttributeValue { N = id.ToString() } } }
+            };
+
+            var response = await dynamoDb.GetItemAsync(request);
+
+            if (!response.IsItemSet)
+                return NotFound();
+
+            return response.Item["Title"].S;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] PostInput input)
+        {
+            var request = new PutItemRequest
+            {
+                TableName = TableName,
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    { "Id", new AttributeValue { N = input.Id.ToString() }},
+                    { "Title", new AttributeValue { S = input.Title }}
+                }
+            };
+
+            await dynamoDb.PutItemAsync(request);
+        }
+    }
+
+    public class PostInput
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace dynamodb_test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var dynamoDbConfig = Configuration.GetSection("DynamoDb");
+            var runLocalDynamoDb = dynamoDbConfig.GetValue<bool>("LocalMode");
+
+            // Quick n Dirty Fix for AWS Creds:
+            Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", "xxx");
+            Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "yyy");
+            Environment.SetEnvironmentVariable("AWS_REGION", "us-east-1");
+
+            // Long Term Fix for AWS Creds:
+            // https://github.com/aws/aws-sdk-net/issues/499
+            // Specify location and setup AWS creds file in configuration files
+            // Then set file accordingly
+            // services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+
+
+            if (runLocalDynamoDb)
+            {
+                services.AddSingleton<IAmazonDynamoDB>(sp =>
+                {
+                    var clientConfig = new AmazonDynamoDBConfig()
+                    {
+                        ServiceURL = dynamoDbConfig.GetValue<string>("LocalServiceUrl")
+                    };
+
+                    return new AmazonDynamoDBClient(clientConfig);
+                });
+            }
+            else
+            {
+                services.AddAWSService<IAmazonDynamoDB>();
+            }
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
